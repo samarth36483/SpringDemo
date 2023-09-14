3d99220f54ee5d2692fdfc537f1152d26d809d7e
samarth36483 <raipuriyasamarth@gmail.com>
2023-09-15 00:31:57 +0530

    corrected package name

--- springdemo/src/main/java/com/example/springdemo/controllers/MyFirstController.java
+++ springdemo/src/main/java/com/example/springdemo/controllers/MyFirstController.java
@@ -1,17 +1,17 @@
-package controller;
+package com.example.springdemo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import services.MyFirstService;
+import com.example.springdemo.services.MyFirstService;
 
 @RestController
 public class MyFirstController {
-	private MyFirstService myFirstService;
-	
-	public MyFirstController(MyFirstService myFirstService) {
-		this.myFirstService = myFirstService;
-	}
+//	private MyFirstService myFirstService;
+//	
+//	public MyFirstController(MyFirstService myFirstService) {
+//		this.myFirstService = myFirstService;
+//	}
 	
 	@GetMapping("/")
 	public static String sayHi() {
--- springdemo/src/main/java/com/example/springdemo/services/MyFirstService.java
+++ springdemo/src/main/java/com/example/springdemo/services/MyFirstService.java
@@ -1,4 +1,4 @@
-package services;
+package com.example.springdemo.services;
 
 import org.springframework.stereotype.Service;
 
