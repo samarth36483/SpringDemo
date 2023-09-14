b0315c7e4e5cd79ecbc6ca42f3b0504f1e1a641c
samarth36483 <raipuriyasamarth@gmail.com>
2023-09-15 01:40:58 +0530

    Added RESTful webservice

--- springdemo/src/main/java/com/example/springdemo/controllers/MyFirstController.java
+++ springdemo/src/main/java/com/example/springdemo/controllers/MyFirstController.java
@@ -1,9 +1,12 @@
 package com.example.springdemo.controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springdemo.services.MyFirstService;
+import com.example.springdemo.Greeting;
 
 @RestController
 public class MyFirstController {
@@ -13,9 +16,12 @@ public class MyFirstController {
 //		this.myFirstService = myFirstService;
 //	}
 	
-	@GetMapping("/")
-	public static String sayHi() {
-		return "Hey there!";
+	private static String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
 }
